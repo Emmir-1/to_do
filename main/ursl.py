0638197c5162e2a0516fe63a9c4cc7c3155f62e9
Emmir-1 <emmir.se@gmail.com>
2023-06-16 20:59:21 +0600

    new2

---
+++ main/ursl.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import tasks_create_list_view, task_detail_view, TaskListCreateAPIView, TaskDetailAPView, \
+    TaskListCreateView, TaskDetailView
+
+urlpatterns = [
+    #functionview
+    path('tasks/', tasks_create_list_view),  # GET / POST
+    path('tasks/<int:pk>/', task_detail_view), # GET / PUT PATCH / DELETE
+
+    #generics
+    path('class/tasks/', TaskListCreateView.as_view()),
+    path('class/tasks/<int:pk>/', TaskDetailView.as_view()),
+
+    #apiview
+
+    # path('class/tasks/', TaskListCreateAPIView.as_view()),
+    # path('class/tasks/<int:pk>/', TaskDetailAPView.as_view()),
+
+]
+
+# 127.0.0.1:8000/api/v1/tasks/ -> GET / POST tasks_create_list_view
+# 127.0.0.1:8000/api/v1/tasks/id/ -> GET/PUT/PATCH/DELETE tasks_detail_view
